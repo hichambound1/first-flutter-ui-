d83b2b1f5e1c19f0f3f990b374a7efa6d746b03b
Hicham.BOUNDOUQ <hicham.boundouq@um6p.ma>
2022-10-30 08:16:42 +0100

    add spalsh

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 // ignore_for_file: prefer_const_constructors, duplicate_ignore
 import 'package:flutter/material.dart';
 import 'package:flutter_application/ui/pages/social.dart';
-import 'package:flutter_application/ui/pages/splash.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    // ignore_for_file: avoid_print
+    
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
 
   // This widget is the root of your application.
   @override
